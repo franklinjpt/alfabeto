4a7c6bdf161ed4388ea7fa475bbcc442418a86ec
Franklin Pineda <franklinpinedat@gmail.com>
2020-09-26 17:37:53 -0500

    Agregado la función Union

--- js/logic.js
+++ js/logic.js
@@ -1,32 +1,64 @@
 //Variables
-const inputAlfabeto = document.querySelector('#alfabeto');
+const inputAlfabeto1 = document.querySelector('#alfabeto1');
+const inputAlfabeto2 = document.querySelector('#alfabeto2');
 const btnAlfabeto = document.querySelector('button[type="submit"]');
 const formulario = document.querySelector('#formulario');
- 
+const mostrar1 = document.querySelector('.mostrarAlfabeto1');
+const mostrar2 = document.querySelector('.mostrarAlfabeto2');
+const aux = document.querySelector('.aux');
+const enlaceUnion = document.querySelector('#union')
+let valorAlfabeto1 = null;
+let valorAlfabeto2 = null;
+
 
 
 //Listeners
 cargarEventListeners();
+mostrarAlfabeto();
 function cargarEventListeners(){
 
     formulario.addEventListener('submit', leerAlfabeto);
 }
 
-
-
-
 //Funciones
 function leerAlfabeto(e){
     e.preventDefault();
-    let alfabetoLeido = alfabeto.value;
-    if(alfabetoLeido === ''){
+    let alfabeto1 = inputAlfabeto1.value;
+    let alfabeto2 = inputAlfabeto2.value;
+
+    if(alfabeto1 === '' && alfabeto2 === ''){
         alert('El formulario no puede estar vacio')
     }else{
-        //Falta que se agregue otro alfabeto
-        let nuevoalfabeto = [];
-        nuevoalfabeto = alfabetoLeido.split(",");
-        localStorage.setItem('alfabeto', JSON.stringify(nuevoalfabeto));
+        let nuevoalfabeto1,nuevoalfabeto2 = [];
+        nuevoalfabeto1 = alfabeto1.split(",");
+        nuevoalfabeto2 = alfabeto2.split(",");
+        localStorage.setItem('alfabeto1', JSON.stringify(nuevoalfabeto1));
+        localStorage.setItem('alfabeto2', JSON.stringify(nuevoalfabeto2));
+        valorAlfabeto1 = JSON.parse(localStorage.getItem('alfabeto1'));
+        valorAlfabeto2 = JSON.parse(localStorage.getItem('alfabeto2'));
+        
+        mostrarAlfabeto();
+        mostrarUnion();
+    }
+    inputAlfabeto1.value = '';
+    inputAlfabeto2.value = '';
+}
+
+function mostrarAlfabeto(){
+
+    if(valorAlfabeto1 !== null && valorAlfabeto2 !== null){
+        aux.remove();
+        mostrar1.textContent = `A1: { ${valorAlfabeto1}} `;
+        mostrar2.textContent = `A2: {${valorAlfabeto2}}`;
 
     }
-    inputAlfabeto.value = '';
+}
+
+
+
+function mostrarUnion(){
+    const alfabetosPegados = valorAlfabeto1.concat(valorAlfabeto2);
+    const filtroUnion = ((el,index) => alfabetosPegados.indexOf(el) === index);
+    const unionAlfabetos = alfabetosPegados.filter(filtroUnion);
+    enlaceUnion.textContent= `{${unionAlfabetos}}`;
 }
