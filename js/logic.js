242b76bd0ad340cdd3237817c60031753caf353f
Franklin Pineda <franklinpinedat@gmail.com>
2020-09-29 06:25:06 -0500

    Eliminado el local storage y arreglando la vista

--- js/logic.js
+++ js/logic.js
@@ -10,12 +10,12 @@ const enlaceUnion = document.querySelector('#union');
 const enlaceInter = document.querySelector('#interseccion');
 const enlaceDiff = document.querySelector('#diferencia');
 
-let valorAlfabeto1 = null;
-let valorAlfabeto2 = null;
+let valorAlfabeto1 = [];
+let valorAlfabeto2 = [];
+let alfabetos = [];
 
 //Listeners
 cargarEventListeners();
-mostrarAlfabeto();
 function cargarEventListeners(){
 
     formulario.addEventListener('submit', leerAlfabeto);
@@ -24,32 +24,20 @@ function cargarEventListeners(){
 //Funciones
 function leerAlfabeto(e){
     e.preventDefault();
-    let alfabeto1 = inputAlfabeto1.value;
-    let alfabeto2 = inputAlfabeto2.value;
+    
+    valorAlfabeto1 = inputAlfabeto1.value.split(",");
+    valorAlfabeto2 = inputAlfabeto2.value.split(",");
+    alfabetos = valorAlfabeto1.concat(valorAlfabeto2);
 
-    if(alfabeto1 === '' && alfabeto2 === ''){
-        alert('El formulario no puede estar vacio')
-    }else{
-        let nuevoalfabeto1,nuevoalfabeto2 = [];
-        nuevoalfabeto1 = alfabeto1.split(",");
-        nuevoalfabeto2 = alfabeto2.split(",");
-        localStorage.setItem('alfabeto1', JSON.stringify(nuevoalfabeto1));
-        localStorage.setItem('alfabeto2', JSON.stringify(nuevoalfabeto2));
-        valorAlfabeto1 = JSON.parse(localStorage.getItem('alfabeto1'));
-        valorAlfabeto2 = JSON.parse(localStorage.getItem('alfabeto2'));
+    mostrarAlfabeto();
+    mostrarUnion();
+    mostrarInterseccion();
+    mostrarDiferencia();
 
-        mostrarAlfabeto();
-        mostrarUnion();
-        mostrarInterseccion();
-        mostrarDiferencia();
-    }
-    inputAlfabeto1.value = '';
-    inputAlfabeto2.value = '';
 }
 
 function mostrarAlfabeto(){
-
-    if(valorAlfabeto1 !== null && valorAlfabeto2 !== null){
+    if(valorAlfabeto1 !== '' && valorAlfabeto2 !== ''){
         aux.remove();
         mostrar1.textContent = `A1: {${valorAlfabeto1}}`;
         mostrar2.textContent = `A2: {${valorAlfabeto2}}`;
@@ -57,16 +45,14 @@ function mostrarAlfabeto(){
 }
 
 function mostrarUnion(){
-    const alfabetosPegados = valorAlfabeto1.concat(valorAlfabeto2);
-    const filtroUnion = ((el,index) => alfabetosPegados.indexOf(el) === index);
-    const unionAlfabetos = alfabetosPegados.filter(filtroUnion);
+    const filtroUnion = ((el,index) => alfabetos.indexOf(el) === index);
+    const unionAlfabetos = alfabetos.filter(filtroUnion);
     enlaceUnion.textContent = `{${unionAlfabetos}}`;
 }
 
 function mostrarInterseccion(){
-    const alfabetosInter = valorAlfabeto1.concat(valorAlfabeto2);
-    const filtroInter = ((el,index) => alfabetosInter.indexOf(el) !== index);
-    const interseccionAlfa = alfabetosInter.filter(filtroInter);
+    const filtroInter = ((el,index) => alfabetos.indexOf(el) !== index);
+    const interseccionAlfa = alfabetos.filter(filtroInter);
     enlaceInter.textContent = `{${interseccionAlfa}}`;
 }
 
