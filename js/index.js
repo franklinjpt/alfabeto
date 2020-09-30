c43679ea2f259331f8bab37dd4930dc53114bac0
Franklin Pineda <franklinpinedat@gmail.com>
2020-09-29 20:27:10 -0500

    Agregada la funcion potencia

--- js/index.js
+++ js/index.js
@@ -20,16 +20,26 @@ function mostrarDiferencia(){
     return `{${diferencia}}`;
 }
 
-function concatenacion(){
-    let prueba = [];
-    for(let i=0; i<valorInput1.length; i++){
-        let a = valorInput1[i];
-        //a = aaa
-        for(let j=0; j<valorInput2.length; j++){
-            let b = valorInput2[j];
-            //b = bbb
-            (a === '!' ? prueba.push(b) : b === '!' ? prueba.push(a) : prueba.push(`${a+b}`) );
+function concatenacion(valor1, valor2){
+    let concatenacion = [];
+    for(let i=0; i<valor1.length; i++){
+        let a = valor1[i];
+        for(let j=0; j<valor2.length; j++){
+            let b = valor2[j];
+            (a === '!' ? concatenacion.push(b) : b === '!' ? concatenacion.push(a) : concatenacion.push(`${a + b}`) );
         }
     }
-    return `{${prueba}}`;
+    return concatenacion;
 }
+
+function potencia(otra ,nPotencia) {
+    let lgPotencia = [...otra];
+    console.log(nPotencia);
+    for (let i = 0; i < nPotencia; i++) {
+      lgPotencia = concatenacion(lgPotencia, otra);
+    }
+    return lgPotencia;
+  }
+
+
+
--- js/lenguaje.js
+++ js/lenguaje.js
@@ -6,10 +6,12 @@ const formulario = document.querySelector('#formulario');
 const mostrar1 = document.querySelector('.mostrarLenguaje1');
 const mostrar2 = document.querySelector('.mostrarLenguaje2');
 const aux = document.querySelector('.aux');
+let nPotencia = parseInt(document.querySelector('#nPotencia').value);
 const enlaceUnion = document.querySelector('#union');
 const enlaceInter = document.querySelector('#interseccion');
 const enlaceDiff = document.querySelector('#diferencia');
 const enlaceConcat = document.querySelector('#concatenacion');
+const enlacePoten = document.querySelector('#potencia');
 
 //Listeners
 cargarEventListeners();
@@ -24,12 +26,8 @@ function leerLenguaje(e){
     valorInput1= inputLenguaje1.value.split(",");
     valorInput2 = inputLenguaje2.value.split(",");
     inputs = valorInput1.concat(valorInput2);
-
+    nPotencia = parseInt(document.querySelector('#nPotencia').value);
     mostrarLenguaje();
-    mostrarUnion();
-    mostrarInterseccion();
-    mostrarDiferencia();
-    concatenacion();
 }
 
 function mostrarLenguaje(){
@@ -40,6 +38,7 @@ function mostrarLenguaje(){
         enlaceUnion.textContent = mostrarUnion();
         enlaceInter.textContent = mostrarInterseccion();
         enlaceDiff.textContent = mostrarDiferencia();
-        enlaceConcat.textContent = concatenacion();
+        enlaceConcat.textContent = `{${concatenacion(valorInput1,valorInput2)}}`;
+        enlacePoten.textContent = `{${potencia(valorInput1,nPotencia - 1)}}`;
     }
 }
